047a1cd3e27487872ed8968eb40a50a3257f7b8d
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2026-02-18 18:15:38 -0600

    chore: implement logic to colelct command-line arguments using flag
    make use of in-built golang package to collect argument from the ccommand line

--- project-meera/main.go
+++ project-meera/main.go
@@ -2,25 +2,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func main() {
-	fmt.Printf("%s", os.Args[1:])
+type Arg int
 
-	filePath := os.Args[1:][0]
-	// 1. Get the data from the CLI
-	fileInfo, err := os.Stat(filePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Printf("Path to file does not exist: %s", filePath)
-			return
-		}
-		fmt.Printf("Some error occurred: %s", err)
-		return
+const (
+	Invalid Arg = iota - 1
+	Help
+	Path
+)
+
+func (a Arg) String() string {
+	return [...]string{"Help", "Path", "Invalid"}[a]
+}
+
+func ParseArgs(a string) (Arg, error) {
+	switch strings.ToLower(a) {
+	case "help":
+		return Help, nil
+	case "path":
+		return Path, nil
+	default:
+		return Invalid, fmt.Errorf("invalid Argument provided: %q", a)
 	}
-	fmt.Print(fileInfo)
-	// 2. Check if the given arg is a path/file
-	// 3. Use the given arg to determine if we need to get info on the video
+
+}
+
+func (a *Arg) Set(val string) error {
+	parsed, err := ParseArgs(val)
+
+	if err != nil {
+		return err
+	}
+
+	*a = parsed
+
+	return nil
+}
+
+func main() {
+	var arg string
+	flag.StringVar(&arg, "path", "", "Search path")
+	if err := flag.CommandLine.Parse(os.Args[1:]); err != nil {
+		if err == flag.ErrHelp {
+			flag.Usage()
+			os.Exit(1)
+		}
+		fmt.Println("Error:", err)
+		os.Exit(2)
+	}
+
+	fmt.Printf("%v", arg)
+	fmt.Println("searching...")
+
 }
